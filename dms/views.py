4b4ce5a3a2d9d43e296e28d94db058a8afe1f51b
Tomasz Gaładyk <tomasz.galadyk@gmail.com>
2019-03-15 07:36:41 +0100

    created index controller

--- dms/views.py
+++ dms/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Department
 
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    departments_list = Department.objects.all()
+    context = { 'departments_list' : departments_list}
+
+    return render(request, context=context, template_name='index')
