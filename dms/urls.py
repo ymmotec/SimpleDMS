f0c85e7442c7397c153e799309d3a1ff0b5e7091
Tomasz Gaładyk <tomasz.galadyk@gmail.com>
2019-03-16 21:53:59 +0100

    fixed bug with views

--- dms/urls.py
+++ dms/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import department
 
 app_name = 'dms'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', department.index, name='index'),
 ]
