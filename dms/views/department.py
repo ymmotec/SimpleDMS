2ebf358e3e3e3accd90ca957feed10ce47c3c78c
Tomasz Gaładyk <tomasz.galadyk@gmail.com>
2019-03-19 21:55:17 +0100

    Created details controller for department

--- dms/views/department.py
+++ dms/views/department.py
@@ -11,3 +11,8 @@ def index(request):
     context = { 'departments_list' : departments_list}
 
     return render(request, 'dms/index.html', context)
+
+
+def details(request, department_id):
+    department = get_object_or_404(Department, pk=department_id)
+    return render(request, 'dms/department_details.html', {'department': department})
