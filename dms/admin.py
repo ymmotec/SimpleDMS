80367b98159ce8dff7590fe102f521b7c9e2d9d9
Tomasz Gaładyk <tomasz.galadyk@gmail.com>
2019-03-29 22:41:54 +0100

    Created DocumentAdmin class for admin site form

--- dms/admin.py
+++ dms/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from .models import Department, Category, Document
 
-admin.site.register(Department)
+class DocumentAdmin(admin.ModelAdmin):
+    fields = (
+        ('title', 'document_number'),
+        'category',
+        'product',
+        'department'
+    )
+
+    filter_horizontal = ['product', 'department']
+
+#admin.site.register(Department)
 admin.site.register(Category)
 admin.site.register(Document)
